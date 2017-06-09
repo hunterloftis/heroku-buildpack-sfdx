647ec7b074ef85e1a07a19398096701661df7619
Hunter Loftis <hunter@hunterloftis.com>
2017-06-09 10:45:23 -0400

    save org url and redirect from the heroku app

--- app/server.js
+++ app/server.js
@@ -1,6 +1,10 @@
 const http = require('http')
+const fs = require('fs')
+const path = require('path')
 const PORT = process.env.PORT || 3000
+const URL = fs.readFileSync(path.join(__dirname, 'force-org-open.txt'), 'utf8')
 
 http.createServer((req, res) => {
-  res.end('Hello')
+  res.writeHead(302, { Location: URL })
+  res.end()
 }).listen(PORT, () => console.log(`Listening on ${PORT}`))
