1e5e728e6bfb5515aab91a3ea490a9cebd51f963
Hunter Loftis <hunter@hunterloftis.com>
2017-06-16 15:06:53 -0700

    simpler configuration

--- app/server.js
+++ app/server.js
@@ -1,6 +1,11 @@
 const http = require('http')
+const fs = require('fs')
+const path = require('path')
 const PORT = process.env.PORT || 3000
+const URL = fs.readFileSync(path.join(__dirname, 'force-org-open.txt'), 'utf8').trim()
 
 http.createServer((req, res) => {
-  res.end('Hello')
+  console.log('Location:', URL)
+  res.writeHead(302, { Location: URL })
+  res.end()
 }).listen(PORT, () => console.log(`Listening on ${PORT}`))
