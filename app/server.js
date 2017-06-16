0e5930617010aa6a40682ed3dff833d25b3bc22e
Hunter Loftis <hunter@hunterloftis.com>
2017-06-16 14:56:05 -0700

    trim file

--- app/server.js
+++ app/server.js
@@ -2,7 +2,7 @@ const http = require('http')
 const fs = require('fs')
 const path = require('path')
 const PORT = process.env.PORT || 3000
-const URL = fs.readFileSync(path.join(__dirname, 'force-org-open.txt'), 'utf8')
+const URL = fs.readFileSync(path.join(__dirname, 'force-org-open.txt'), 'utf8').trim()
 
 http.createServer((req, res) => {
   console.log('Location:', URL)
