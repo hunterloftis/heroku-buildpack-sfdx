c7dad23001d6a88b3c4bf196b013b3a2478b7b57
Hunter Loftis <hunter@hunterloftis.com>
2017-06-16 14:52:48 -0700

    log header

--- app/server.js
+++ app/server.js
@@ -5,6 +5,7 @@ const PORT = process.env.PORT || 3000
 const URL = fs.readFileSync(path.join(__dirname, 'force-org-open.txt'), 'utf8')
 
 http.createServer((req, res) => {
+  console.log('Location:', URL)
   res.writeHead(302, { Location: URL })
   res.end()
 }).listen(PORT, () => console.log(`Listening on ${PORT}`))
